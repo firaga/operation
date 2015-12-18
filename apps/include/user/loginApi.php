9bb64b12b2ff8a5532ddf0591cb2fd622da428a3
jichen <zhou.jichen@gmail.com>
2015-12-18 15:43:24 +0800

    登陆

--- apps/include/user/loginApi.php
+++ apps/include/user/loginApi.php
@@ -1,44 +1,52 @@
 <?php
+
 /**
  * Created by JetBrains PhpStorm.
  * User: Jichen Zhou
  * Date: 2015年12月14日
  * Time: 下午6:22
  */
-class KUser_loginApi extends Ko_Mode_Item{
+class KUser_loginApi extends Ko_Mode_Item {
 	protected $_aConf = array(
-			'item' => 'user',
+		'item' => 'user',
 	);
-	const ERRNO_UNREGISTERED=1;
-	const ERRNO_PASSWD=2;
-	public function iLogin($sUsername,$sPasswd,&$iErrNo=0) {
-		if(!$aUserInfo=$this->aIsRegistered($sUsername)){
-			$iErrNo=self::ERRNO_UNREGISTERED;
+	const ERRNO_UNREGISTERED = 1;
+	const ERRNO_PASSWD = 2;
+
+	public function iLogin($sUsername, $sPasswd, &$iErrNo = 0) {
+		if (!$aUserInfo = $this->aIsRegistered($sUsername)) {
+			$iErrNo = self::ERRNO_UNREGISTERED;
 			return 0;
 		}
-		if(md5($aUserInfo['salt'].'_'.$sPasswd)!=$aUserInfo['passwd']){
-			$iErrNo=self::ERRNO_UNREGISTERED;
+		if (md5($aUserInfo['salt'] . '_' . $sPasswd) != $aUserInfo['passwd']) {
+			$iErrNo = self::ERRNO_UNREGISTERED;
 			return 0;
 		}
 		//setcookie
+		setcookie('uid',$aUserInfo['id']);
 		return $aUserInfo['id'];
 	}
 
-	public function aIsRegistered($aUsername){
+	public function aIsRegistered($aUsername) {
 		$aOption = new Ko_Tool_SQL();
-		$aOption->oWhere(array('username'=>$aUsername));
-		$aList=$this->aGetList($aOption,1000);
+		$aOption->oWhere(array('username' => $aUsername));
+		$aList = $this->aGetList($aOption, 1000);
 		return $aList[0];
 	}
 
-	public function bRegister($sUsername,$sPasswd,&$iErrNo=0) {
-		$sSalt=rand(1000,9999);
-		$aData=array(
-				'username'=>$sUsername,
-				'passwd'=>md5($sSalt.'_'.$sPasswd),
-				'salt'=>$sSalt,
+	public function bRegister($sUsername, $sPasswd, &$iErrNo = 0) {
+		$sSalt = rand(1000, 9999);
+		$aData = array(
+			'username' => $sUsername,
+			'passwd'   => md5($sSalt . '_' . $sPasswd),
+			'salt'     => $sSalt,
 		);
-		$iUid=$this->iInsert($aData);
+		$iUid = $this->iInsert($aData);
 		return !!$iUid;
+	}
+
+	public function iGetLoginUid() {
+		return $_COOKIE['uid'];
+
 	}
 }
