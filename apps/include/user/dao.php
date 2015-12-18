27deb203bd6977e9ac84242cadfd84d6a9dbe6ca
jichen <zhou.jichen@gmail.com>
2015-12-18 16:49:42 +0800

    sql

--- apps/include/user/dao.php
+++ apps/include/user/dao.php
@@ -5,7 +5,7 @@
  * Date: 2015年12月14日
  * Time: 下午7:09
  */
-class KQa_Dao extends Ko_Dao_Factory {
+class KUser_Dao extends Ko_Dao_Factory {
 	public $_aDaoConf = array(
 		'redis'  => array(
 			'type' => 'redis'
