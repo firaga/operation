026f69979eb6e5e8f970508a0d5030be4d58c7d3
jichen <zhou.jichen@gmail.com>
2015-12-23 16:58:00 +0800

    引入bootstrap

--- apps/include/render/web.php
+++ apps/include/render/web.php
@@ -11,17 +11,18 @@ class KRender_web extends KRender_base
 	{
 		$loginApi = new KUser_loginApi;
 		$uid = $loginApi->iGetLoginUid();
-		$logininfo = $uid ? Ko_Tool_Adapter::VConv($uid, array('user_baseinfo', array('logo32'))) : array();
+		$userApi=new KUser_userApi();
+		$logininfo=$userApi->getUser($uid);
 
 		$head = new Ko_View_Render_Smarty;
-		$head->oSetTemplate('default/common/header.html')
+		$head->oSetTemplate('operation/common/header.html')
 			->oSetData('IMG_DOMAIN', IMG_DOMAIN)
 			->oSetData('WWW_DOMAIN', WWW_DOMAIN)
 			->oSetData('PASSPORT_DOMAIN', PASSPORT_DOMAIN)
 			->oSetData('logininfo', $logininfo);
 
 		$tail = new Ko_View_Render_Smarty;
-		$tail->oSetTemplate('default/common/footer.html');
+		$tail->oSetTemplate('operation/common/footer.html');
 
 		return $head->sRender().parent::sRender().$tail->sRender();
 	}
