a1e10638e623623972dafce1eb9a362386a5e6b7
jichen <zhou.jichen@gmail.com>
2015-12-24 18:08:24 +0800

    顶部导航管理

--- apps/include/render/web.php
+++ apps/include/render/web.php
@@ -1,20 +1,19 @@
 <?php
+
 /**
  * Created by JetBrains PhpStorm.
  * User: Jichen Zhou
  * Date: 2015年12月23日
  * Time: 下午3:22
  */
-class KRender_web extends KRender_base
-{
-	public function sRender()
-	{
-		$uid=$_SESSION['admin']['id'];
-		$userApi=new KUser_userApi();
-		$logininfo=$userApi->getUser($uid);
+class KRender_web extends KRender_base {
+	public function sRender() {
+		$uid = $_SESSION['admin']['id'];
+		$userApi = new KUser_userApi();
+		$logininfo = $userApi->getUser($uid);
 		$app = new KOperation_App();
 
-		$head=new Ko_View_Smarty();
+		$head = new Ko_View_Smarty();
 		$head->vAssignHtml('IMG_DOMAIN', IMG_DOMAIN);
 		$head->vAssignHtml('WWW_DOMAIN', WWW_DOMAIN);
 		$head->vAssignHtml('PASSPORT_DOMAIN', PASSPORT_DOMAIN);
@@ -24,19 +23,22 @@ class KRender_web extends KRender_base
 		$head->vAssignHtml('__top_menus', $top_menus);
 		$head->vAssignHtml('__cur_menus', $cur_menu);
 		$head->vAssignRaw('__admin_nav', $left_nav_html);
-		$head->vAssignRaw('is_super', !!in_array($logininfo['id'],KOperation_Conf::$super_users));
+		$head->vAssignRaw('is_super', !!in_array($logininfo['id'], KOperation_Conf::$super_users));
 
-		$headHtml=$head->sFetch('operation/common/header.html');
-//		$head = new Ko_View_Render_Smarty;
-//		$head->oSetTemplate('operation/common/header.html')
-//			->oSetData('IMG_DOMAIN', IMG_DOMAIN)
-//			->oSetData('WWW_DOMAIN', WWW_DOMAIN)
-//			->oSetData('PASSPORT_DOMAIN', PASSPORT_DOMAIN)
-//			->oSetData('logininfo', $logininfo);
+		$headHtml = $head->sFetch('operation/common/header.html');
+		//		$head = new Ko_View_Render_Smarty;
+		//		$head->oSetTemplate('operation/common/header.html')
+		//			->oSetData('IMG_DOMAIN', IMG_DOMAIN)
+		//			->oSetData('WWW_DOMAIN', WWW_DOMAIN)
+		//			->oSetData('PASSPORT_DOMAIN', PASSPORT_DOMAIN)
+		//			->oSetData('logininfo', $logininfo);
 
 		$tail = new Ko_View_Render_Smarty;
-		$tail->oSetTemplate('operation/common/footer.html');
+		$tail->oSetTemplate('operation/common/footer.html')
+			->oSetData('IMG_DOMAIN', IMG_DOMAIN)
+			->oSetData('WWW_DOMAIN', WWW_DOMAIN)
+			->oSetData('PASSPORT_DOMAIN', PASSPORT_DOMAIN);
 
-		return $headHtml.parent::sRender().$tail->sRender();
+		return $headHtml . parent::sRender() . $tail->sRender();
 	}
 }
