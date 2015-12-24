ab5b451d9c7b39269fed4802181dbe806bc01595
jichen <zhou.jichen@gmail.com>
2015-12-24 18:24:48 +0800

    结构调整

--- apps/www/operation/index.php
+++ apps/www/operation/index.php
@@ -1,11 +1,4 @@
 <?php
-Ko_Web_Route::VGet('index', function () {
-	$msg = 'hello world!';
-	$render = new KRender_web;
-	$render->oSetTemplate('operation/index.html')
-//		->oSetData('msg', $msg)
-		->oSend();
-	//var_dump(debug_backtrace());
-	//$userApi=new KUser_userApi();
-	//$res=$userApi->checkLogin();
-});
+$render = new KRender_web;
+$render->oSetTemplate('operation/index.html')
+	->oSend();
---
+++ apps/www/operation/system/user.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Jichen Zhou
+ * Date: 2015年12月24日
+ * Time: 下午6:19
+ */
+Ko_Web_Route::VGet('index', function () {
+	$msg = 'hello world';
+	$render = new KRender_web;
+	$render->oSetTemplate('operation/menu/list.tpl')
+		->oSetData('msg', $msg)
+		->oSend();
+});
